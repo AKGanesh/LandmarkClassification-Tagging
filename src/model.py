177f16c784fe936ed5c292326d9f71ef2588cf29
CMIGanesh <ganesh.a@comakeit.com>
2023-06-02 14:56:13 +0530

    Project structure

---
+++ src/model.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+
+
+# define the CNN architecture
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+
+        super().__init__()
+
+        # YOUR CODE HERE
+        # Define a CNN architecture. Remember to use the variable num_classes
+        # to size appropriately the output of your classifier, and if you use
+        # the Dropout layer, use the variable "dropout" to indicate how much
+        # to use (like nn.Dropout(p=dropout))
+        
+        self.model = nn.Sequential(
+            # First conv + maxpool + relu (64x224x224)
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2),
+            #nn.Dropout2d(0.2),
+            
+            # Second conv + maxpool + relu (128x112X112)
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2, 2),
+            #nn.Dropout2d(0.2),
+            
+            # Third conv + maxpool + relu (256x56x56)
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(),
+            #nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout2d(0.2),
+            
+             
+            # Fourth conv + maxpool + relu (512x28x28)
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(),
+            
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout2d(0.2),
+            
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(),
+            
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout2d(0.2),
+            
+            # Flatten feature maps (512x7x7)
+            nn.Flatten(),
+            
+            # Fully connected layers. This assumes that the input image was 28x28
+            nn.Linear(512*7*7, 256),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            
+            nn.Linear(256, num_classes)
+    )
+        
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # YOUR CODE HERE: process the input tensor through the
+        # feature extractor, the pooling and the final linear
+        # layers (if appropriate for the architecture chosen)
+        return self.model(x)
+
+
+######################################################################################
+#                                     TESTS
+######################################################################################
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_loaders():
+    from .data import get_data_loaders
+
+    return get_data_loaders(batch_size=2)
+
+
+def test_model_construction(data_loaders):
+
+    model = MyModel(num_classes=23, dropout=0.3)
+
+    dataiter = iter(data_loaders["train"])
+    images, labels = dataiter.next()
+
+    out = model(images)
+
+    assert isinstance(
+        out, torch.Tensor
+    ), "The output of the .forward method should be a Tensor of size ([batch_size], [n_classes])"
+
+    assert out.shape == torch.Size(
+        [2, 23]
+    ), f"Expected an output tensor of size (2, 23), got {out.shape}"
